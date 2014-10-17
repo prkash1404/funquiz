c2a2134e026d923aad69fd179c53ce454a47e654
prakash <prkash1404@gmail.com>
2014-10-17 10:39:49 +0530

    pg

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -47,7 +47,7 @@ class QuestionsController < ApplicationController
 
     respond_to do |format|
       if @question.save
-        format.html { redirect_to(@question, :notice => 'Question was successfully created.') }
+        format.html { redirect_to(@question )}#, :notice => 'Question was successfully created.') }
         format.xml  { render :xml => @question, :status => :created, :location => @question }
       else
         format.html { render :action => "new" }
@@ -63,7 +63,7 @@ class QuestionsController < ApplicationController
 
     respond_to do |format|
       if @question.update_attributes(question_params)
-        format.html { redirect_to(@question, :notice => 'Question was successfully updated.') }
+        format.html { redirect_to(@question)}#, :notice => 'Question was successfully updated.') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
